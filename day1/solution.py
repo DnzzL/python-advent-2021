f508239b8f8515bbbe5008d90c28327ff97baabf
Thomas Legrand <legrand.thomas5@hotmail.fr>
2021-12-02 17:29:51 +0100

    add docstring

--- day1/solution.py
+++ day1/solution.py
@@ -1,4 +1,12 @@
 def part_one(data: list) -> int:
+    """count the number of times a depth measurement increases from the previous measurement
+
+    Args:
+        data (list): [input data]
+
+    Returns:
+        int: [result]
+    """
     nb_increase = 0
     for i, j in zip(data, data[1:]):
         if j > i:
@@ -7,6 +15,16 @@ def part_one(data: list) -> int:
 
 
 def part_two(data: list) -> int:
+    """count the number of times the sum of measurements in this sliding window increases from the previous sum.
+    So, compare A with B, then compare B with C, then C with D, and so on.
+    Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+    Args:
+        data (list): [input data]
+
+    Returns:
+        int: [result]
+    """
     nb_increase = 0
     previous_sum = None
     for i, j, k in zip(data, data[1:], data[2:]):
--- day2/solution.py
+++ day2/solution.py
@@ -1,4 +1,13 @@
 def part_one(data: list) -> int:
+    """Calculate the horizontal position and depth you would have after following the planned course.
+    What do you get if you multiply your final horizontal position by your final depth?
+
+    Args:
+        data (list): [input data]
+
+    Returns:
+        int: [result]
+    """
     horizontal_position = 0
     vertical_position = 0
     for instruction, units in data:
@@ -12,6 +21,21 @@ def part_one(data: list) -> int:
 
 
 def part_two(data: list) -> int:
+    """In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0.
+    The commands also mean something entirely different than you first thought:
+
+    down X increases your aim by X units.
+    up X decreases your aim by X units.
+    forward X does two things:
+    It increases your horizontal position by X units.
+    It increases your depth by your aim multiplied by X.
+
+        Args:
+            data (list): [input data]
+
+        Returns:
+            int: [result]
+    """
     horizontal_position = 0
     vertical_position = 0
     aim = 0
